1b5f2424f9f60d08fd4e71a084b2cafe1a353151
Raghav Gaba <158083556+raghavvvgaba@users.noreply.github.com>
2024-08-18 00:46:14 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,28 @@
+console.log('JavaScript begins here');
+
+async function getSongs(){
+    let a = await fetch('http://127.0.0.1:3000/songs/');
+    let response = await a.text(); 
+    let div = document.createElement("div");
+    div.innerHTML = response;
+    let as = div.getElementsByTagName("a")
+    let songs = []
+    for (let index = 0; index < as.length; index++) {
+        const element = as[index];
+        if(element.href.endsWith(".mp3")){
+            songs.push(element.href)
+        }
+    }
+    return songs
+}
+
+async function main(){
+    //Getting the lists of all songs
+    let songs = await getSongs()
+    console.log(songs);
+
+    var audio = new Audio(songs[0]);
+    audio.play(); 
+    
+}
+main()
